be03e8e94c735c37355754373ab02137cd81eaf7
Justin Li <justinlit23@gmail.com>
2022-04-08 23:12:26 -0700

    commit tree 255b979d2ec688a87f83ccc181e597733719580c

--- index.js
+++ index.js
@@ -10,7 +10,33 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+/* Attempt 1
+const capitalizer = (word) => {
+  const capitalizedWord = word.slice(0,1).toUpperCase() + word.slice(1)
+  return capitalizedWord
+}
 
 const titleCased = () => {
-  return tutorials
+      for (let i = 0; i < tutorials.length; i++) {
+        let slicedUpTitle = tutorials[i].split(" ") //splits into an array of substrings
+        slicedUpTitle = slicedUpTitle.map((word) => capitalizer(word)) //Capitalizes the first letter in the word
+        let rejoinedTitle = "" //Creates empty string to add words back into
+        for (let i = 0; i < slicedUpTitle.length; i++) {
+          rejoinedTitle += slicedUpTitle[i]+ " "
+        }
+        rejoinedTitle = rejoinedTitle.slice(0,-1)
+        tutorials[i] = (rejoinedTitle);
+      }
+      return(tutorials)
+}
+for of can be replaced with for loop//for (let i = 0; i < slicedTitle.length; i++) {rejoinedTitle += slicedTitle[i]+ " "}
+*/
+
+const titleCased = () => {
+  return tutorials.map (title => {
+  const slicedTitle = title.split(" ").map((word) => word.slice(0,1).toUpperCase() + word.slice(1))
+  let rejoinedTitle = ""
+  for (const word of slicedTitle) {rejoinedTitle += word + " "}
+  return rejoinedTitle.slice(0,-1)
+  })
 }
